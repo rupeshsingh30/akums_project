d679bc6a3cd9ac6c9f89427b9815248736ef8b24
Rupesh <rupeshsingh.choudhary@sequelstring.com>
2025-01-28 17:43:20 +0530

    separte main fuction

---
+++ Main.py
@@ -0,0 +1,37 @@
+import os
+import logging
+import shutil
+from Extractioin import extractionFun
+
+
+def processFilesInDirectoryFun(input_folder,error_folder):
+    """
+    Process all files in a given folder.
+    If an error occurs during processing, move the file to an error folder.
+    """
+    # error_folder = os.path.join(input_folder, "error_files")
+    # os.makedirs(error_folder, exist_ok=True)  # Create error folder if it doesn't exist
+    
+    
+
+    for file_name in os.listdir(input_folder):
+        file_path = os.path.join(input_folder, file_name)
+        try:
+            logging.info(f"Processing file: {file_name}")
+            extractionFun(file_path)
+            logging.info(f"Successfully processed file: {file_name}")
+        except Exception as e:
+            logging.error(f"Error processing file {file_name}: {e}")
+            
+            # Move the problematic file to the error folder
+            error_file_path = os.path.join(error_folder, file_name)
+            shutil.move(file_path, error_file_path)
+            logging.warning(f"Moved file to error folder: {error_file_path}")
+        finally:
+            logging.info('-' * 80)
+
+
+# input_folder = r'C:\Users\Admin\Downloads\akums_oce\lohia_new\input'
+input_folder = r'C:\Users\Admin\Downloads\akums_oce\lohia_new\Invoices'
+error_folder = r'C:\Users\Admin\Downloads\akums_oce\lohia_new\error'
+processFilesInDirectoryFun(input_folder,error_folder)
