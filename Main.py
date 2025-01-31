21b447d57106921ea79b484d0398e25690275c89
Rupesh <rupeshsingh.choudhary@sequelstring.com>
2025-01-31 16:34:44 +0530

    first commit

--- Main.py
+++ Main.py
@@ -16,9 +16,13 @@ def processFilesInDirectoryFun(input_folder,error_folder):
 
     for file_name in os.listdir(input_folder):
         file_path = os.path.join(input_folder, file_name)
+
+        # extractionFun(file_path)
         try:
             logging.info(f"Processing file: {file_name}")
             extractionFun(file_path)
+            pass_path = os.path.join(pass_folder, file_name)
+            shutil.move(file_path, pass_path)
             logging.info(f"Successfully processed file: {file_name}")
         except Exception as e:
             logging.error(f"Error processing file {file_name}: {e}")
@@ -31,7 +35,14 @@ def processFilesInDirectoryFun(input_folder,error_folder):
             logging.info('-' * 80)
 
 
-# input_folder = r'C:\Users\Admin\Downloads\akums_oce\lohia_new\input'
+# input_folder = r"C:\Users\rupes\Downloads\akums_ocr\input"
 input_folder = r'C:\Users\Admin\Downloads\akums_oce\lohia_new\Invoices'
-error_folder = r'C:\Users\Admin\Downloads\akums_oce\lohia_new\error'
+error_folder = r"C:\Users\Admin\Downloads\akums_oce\lohia_new\error"
+pass_folder = r"C:\Users\Admin\Downloads\akums_oce\lohia_new\pass"
 processFilesInDirectoryFun(input_folder,error_folder)
+
+
+
+
+
+
